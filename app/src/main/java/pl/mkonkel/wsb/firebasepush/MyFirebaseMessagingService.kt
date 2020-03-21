dab4b231632e430f2fbdc26f9c242e7973ce7984
Michał Konkel <konkel.michal@gmail.com>
2020-03-21 12:57:04 +0100

    App will start Detail Activity when push is clicked. The activity will present the title and body of push

--- app/src/main/java/pl/mkonkel/wsb/firebasepush/MyFirebaseMessagingService.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasepush/MyFirebaseMessagingService.kt
@@ -3,7 +3,9 @@ package pl.mkonkel.wsb.firebasepush
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -19,13 +21,14 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onMessageReceived(remoteMessage)
 
         val title = remoteMessage.notification?.title ?: "empty title"
-
         val message = remoteMessage.notification?.body ?: "empty message"
 
         Log.d("MESSAGE", "title: $title / message: $message ")
 
         createNotificationChannel()
-        val notification = buildNotification(title)
+
+        val pendingIntent = createPendingIntent(title, message)
+        val notification = buildNotification(title, pendingIntent)
 
         notificationManager.notify(1, notification)
     }
@@ -48,18 +51,31 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
 
-    private fun buildNotification(notificationTitle: String): Notification {
+    private fun buildNotification(notificationTitle: String, pendingIntent: PendingIntent): Notification {
         return Notification.Builder(applicationContext, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
             .setContentTitle("UWAGA!")
             .setContentText(notificationTitle)
             .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
             .build()
     }
 
 
-//    TODO: Add a helper method for creating the Pending Intent that will allow us to run some activity
-//    If you want to pass the notification to the Activity you must use the Extras
+    private fun createPendingIntent(title: String?, message: String?): PendingIntent {
+        val resultIntent = Intent(this, DetailActivity::class.java)
+
+        resultIntent.putExtra(NOTIFICATION_MESSAGE_TITLE, title)
+        resultIntent.putExtra(NOTIFICATION_MESSAGE_BODY, message)
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        return PendingIntent.getActivity(
+            this,
+            1,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
 
     companion object {
         const val NOTIFICATION_MESSAGE_TITLE = "message_title"
